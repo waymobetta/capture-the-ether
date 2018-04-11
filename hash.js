0a49225e84aaab9699b2949d4a779e8d8cae625a
waymobetta <jbroethke@gmail.com>
2018-04-11 11:51:09 -0700

    initial commit

---
+++ hash.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+const fs = require('fs')
+const {sha3} = require('ethereumjs-util')
+const crypto = require('crypto')
+
+for (let i = 0; i < 255; i++) {
+	var keccak256hash = sha3(i).toString('hex')
+	if (keccak256hash == "db81b4d58595fbbbb592d3661a34cdca14d7ab379441400cbfa1b78bc447c365") {
+		console.log("[*] Winner: ", i)
+	}
+}
